e4b3387a13759d8f20f7d61243d215f33e435aa6
USER <USER@ROG-553vd>
2018-08-02 12:39:06 +0700

    initial commit

--- src/javaapplication15/JavaApplication15.java
+++ src/javaapplication15/JavaApplication15.java
@@ -22,42 +22,7 @@ public class JavaApplication15 {
     int jumlahBarang;
     double hargaSatuBarang=10000,totalHarga,diskon,totalBayar;
 
-    System.out.println(" ~ Program Kasir ~ ");
-    System.out.print("Nama Pembeli : ");
-    namaPembeli = scan.next();
-    System.out.print("anggota : ");
-    anggota = scan.next();
     
-    System.out.print("Jumlah Barang : ");
-    jumlahBarang = scan.nextInt();
-    
-    //public class void(pembayaran);{
-      totalHarga = hargaSatuBarang * jumlahBarang;
-      
-      if (anggota.equalsIgnoreCase("ya")){
-        if (totalHarga > 1000000){
-        diskon = totalHarga * 10 / 100;
-        totalHarga = totalHarga - diskon;
-        diskon = totalHarga * 10/100;
-        totalHarga = totalHarga - diskon;
-        System.out.println("totalHarga :" +totalHarga);
-        }
-        
-        else if (totalHarga < 1000000){
-        diskon = totalHarga * 10/100;
-        totalHarga = totalHarga - diskon;
-        System.out.println("totalHarga :" +totalHarga);
-        }
-        
-        }
-        if (anggota.equalsIgnoreCase("Tidak")){
-          if (totalHarga >1000000){
-            diskon = totalHarga * 10/100;
-        totalHarga = totalHarga - diskon;
-        System.out.println("totalHarga :" +totalHarga);
-          }
-          else if (totalHarga < 1000000){
-            System.out.println("totalHarga :" + totalHarga);
           }
         }
         
